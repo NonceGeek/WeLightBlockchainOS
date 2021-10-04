82648c8d05281822efce36a8990d1ff8ebc04b85
Suning Yao <sy2940@nyu.edu>
2021-10-04 15:46:27 -0400

    add login of github and metamask auth

--- src/components/screen/booting_screen.js
+++ src/components/screen/booting_screen.js
@@ -1,22 +1,107 @@
-import {urls} from '../../apps.config'
+import Web3 from 'web3';
 
 function BootingScreen(props) {
-    const repo = urls.staticUrl.repo
+  const loginWithMetaMask = () => {
+    props.loginWithOAuth(true);
+  };
 
-    return (
-        <div style={(props.visible || props.isShutDown ? { zIndex: "100" } : { zIndex: "-20" })} className={(props.visible || props.isShutDown ? " visible opacity-100" : " invisible opacity-0 ") + " absolute duration-500 select-none flex flex-col justify-around items-center top-0 right-0 overflow-hidden m-0 p-0 h-screen w-screen bg-black"}>
-            <img className="md:w-1/4 w-1/2" src="./themes/Yaru/status/cof_orange_hex.svg" alt="Ubuntu Logo" />
-            <div className="w-10 h-10 flex justify-center items-center rounded-full outline-none cursor-pointer" onClick={props.turnOn} >
-                {(props.isShutDown
-                    ? <div className="bg-white rounded-full flex justify-center items-center w-10 h-10 hover:bg-gray-300"><img className="w-8" src="./themes/Yaru/status/power-button.svg" alt="Power Button" /></div>
-                    : <img className={" w-10 " + (props.visible ? " animate-spin " : "")} src="./themes/Yaru/status/process-working-symbolic.svg" alt="Ubuntu Process Symbol" />)}
+  const loadWeb3 = async () => {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      loginWithMetaMask();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert(
+        'Non-Ethereum browser detected. You should consider trying MetaMask!',
+      );
+    }
+  };
+
+  return (
+    <div
+      style={
+        props.visible || props.isShutDown
+          ? { zIndex: '100' }
+          : { zIndex: '-20' }
+      }
+      className={
+        (props.visible || props.isShutDown
+          ? ' visible opacity-100'
+          : ' invisible opacity-0 ') +
+        ' absolute duration-500 select-none flex flex-col justify-around items-center top-0 right-0 overflow-hidden m-0 p-0 h-screen w-screen bg-black'
+      }
+    >
+      <img
+        className='md:w-1/4 w-1/2'
+        src='./themes/Yaru/status/cof_orange_hex.svg'
+        alt='Ubuntu Logo'
+      />
+      <div
+        className='w-10 h-10 flex justify-center items-center rounded-full outline-none cursor-pointer'
+        onClick={props.turnOn}
+      >
+        {props.isShutDown ? (
+          <div className='bg-white rounded-full flex justify-center items-center w-10 h-10 hover:bg-gray-300'>
+            <img
+              className='w-8'
+              src='./themes/Yaru/status/power-button.svg'
+              alt='Power Button'
+            />
+          </div>
+        ) : (
+          <img
+            className={' w-10 ' + (props.visible ? ' animate-spin ' : '')}
+            src='./themes/Yaru/status/process-working-symbolic.svg'
+            alt='Ubuntu Process Symbol'
+          />
+        )}
+      </div>
+      {props.isShutDown ? (
+        <></>
+      ) : (
+        <div className='mb-4'>
+          <div
+            className='flex items-center px-5 py-2 mb-1 cursor-pointer'
+            style={{
+              backgroundColor: 'rgba(205,217,229,0.7)',
+              color: '#cdd9e5',
+            }}
+          >
+            <img
+              className='w-10'
+              src='/images/logos/github.png'
+              alt='github logo'
+            />
+            <div className='px-5'>Login with GitHub</div>
+          </div>
+          <div
+            onClick={() => loadWeb3()}
+            className='flex bg-white items-center px-5 py-2 mb-1 cursor-pointer'
+          >
+            <img
+              className='w-10'
+              src='/images/logos/metamask.svg'
+              alt='metamask logo'
+            />
+            <div className='px-5'>Login with MetaMask</div>
+          </div>
+          <div className='text-white py-2'>
+            <div className='text-center'>
+              Or press "Enter" to login in {props.remainingSeconds} seconds
             </div>
-            <img className="md:w-1/5 w-1/2" src="./themes/Yaru/status/ubuntu_white_hex.svg" alt="Ubuntu Name" />
-            <div className="text-white mb-4">
-                <a href={repo} rel="noreferrer noopener" target="_blank" className="underline">github</a>
-            </div>
+          </div>
         </div>
-    )
+      )}
+
+      <img
+        className='md:w-1/5 w-1/2'
+        src='./themes/Yaru/status/ubuntu_white_hex.svg'
+        alt='Ubuntu Name'
+      />
+    </div>
+  );
 }
 
-export default BootingScreen
+export default BootingScreen;
--- src/components/ubuntu.js
+++ src/components/ubuntu.js
@@ -6,121 +6,173 @@ import Navbar from './screen/navbar';
 import ReactGA from 'react-ga';
 
 export default class Ubuntu extends Component {
-	constructor() {
-		super();
-		this.state = {
-			screen_locked: false,
-			bg_image_name: 'wall-2',
-			booting_screen: true,
-			shutDownScreen: false
-		};
-	}
+  constructor() {
+    super();
+    this.state = {
+      screen_locked: false,
+      bg_image_name: 'wall-2',
+      booting_screen: true,
+      shutDownScreen: false,
+      remainingSeconds: 30,
+      remainingSecondsInterval: null,
+    };
+  }
 
-	componentDidMount() {
-		this.getLocalData();
-	}
+  componentDidMount() {
+    this.getLocalData();
+  }
 
-	setTimeOutBootScreen = () => {
-		setTimeout(() => {
-			this.setState({ booting_screen: false });
-		}, 2000);
-	};
+  setTimeOutBootScreen = () => {
+    //   will automatically login in 30 seconds
+    setTimeout(() => {
+      this.setState({ booting_screen: false });
+      setTimeout(() => {
+        this.setState({ remainingSeconds: 30 });
+      }, 2000);
+      clearInterval(this.state.remainingSecondsInterval);
+    }, 30000);
+    this.setIntervalRemainingSecond();
+  };
 
-	getLocalData = () => {
-		// Get Previously selected Background Image
-		let bg_image_name = localStorage.getItem('bg-image');
-		if (bg_image_name !== null && bg_image_name !== undefined) {
-			this.setState({ bg_image_name });
-		}
+  setIntervalRemainingSecond = () => {
+    // set an interval and store the id of interval to the state
+    this.setState({
+      remainingSecondsInterval: setInterval(() => {
+        //   every one second, --remainingSeconds
+        this.setState({ remainingSeconds: this.state.remainingSeconds - 1 });
+      }, 1000),
+    });
+  };
 
-		let booting_screen = localStorage.getItem('booting_screen');
-		if (booting_screen !== null && booting_screen !== undefined) {
-			// user has visited site before
-			this.setState({ booting_screen: false });
-		} else {
-			// user is visiting site for the first time
-			localStorage.setItem('booting_screen', false);
-			this.setTimeOutBootScreen();
-		}
+  loginWithoutAuth = (event) => {
+    if (event.key === 'Enter') {
+      this.setState({ booting_screen: false });
+      //   slightly delay on bring back the default remainingSeconds
+      setTimeout(() => {
+        this.setState({ remainingSeconds: 30 });
+      }, 2000);
+      clearInterval(this.state.remainingSecondsInterval);
+    }
+  };
 
-		// get shutdown state
-		let shut_down = localStorage.getItem('shut-down');
-		if (shut_down !== null && shut_down !== undefined && shut_down === 'true') this.shutDown();
-		else {
-			// Get previous lock screen state
-			let screen_locked = localStorage.getItem('screen-locked');
-			if (screen_locked !== null && screen_locked !== undefined) {
-				this.setState({ screen_locked: screen_locked === 'true' ? true : false });
-			}
-		}
-	};
+  loginWithAuth = (isLoggedIn) => {
+    if (isLoggedIn) {
+      this.setState({ booting_screen: false });
+      setTimeout(() => {
+        this.setState({ remainingSeconds: 30 });
+      }, 2000);
+      clearInterval(this.state.remainingSecondsInterval);
+    }
+  };
 
-	lockScreen = () => {
-		// google analytics
-		ReactGA.pageview('/lock-screen');
-		ReactGA.event({
-			category: `Screen Change`,
-			action: `Set Screen to Locked`
-		});
+  getLocalData = () => {
+    // Get Previously selected Background Image
+    let bg_image_name = localStorage.getItem('bg-image');
+    if (bg_image_name !== null && bg_image_name !== undefined) {
+      this.setState({ bg_image_name });
+    }
 
-		document.getElementById('status-bar').blur();
-		setTimeout(() => {
-			this.setState({ screen_locked: true });
-		}, 100); // waiting for all windows to close (transition-duration)
-		localStorage.setItem('screen-locked', true);
-	};
+    let booting_screen = localStorage.getItem('booting_screen');
+    if (booting_screen !== null && booting_screen !== undefined) {
+      // user has visited site before
+      this.setState({ booting_screen: false });
+    } else {
+      // user is visiting site for the first time
+      localStorage.setItem('booting_screen', false);
+      this.setTimeOutBootScreen();
+    }
 
-	unLockScreen = () => {
-		ReactGA.pageview('/desktop');
+    // get shutdown state
+    let shut_down = localStorage.getItem('shut-down');
+    if (shut_down !== null && shut_down !== undefined && shut_down === 'true')
+      this.shutDown();
+    else {
+      // Get previous lock screen state
+      let screen_locked = localStorage.getItem('screen-locked');
+      if (screen_locked !== null && screen_locked !== undefined) {
+        this.setState({
+          screen_locked: screen_locked === 'true' ? true : false,
+        });
+      }
+    }
+  };
 
-		window.removeEventListener('click', this.unLockScreen);
-		window.removeEventListener('keypress', this.unLockScreen);
+  lockScreen = () => {
+    // google analytics
+    ReactGA.pageview('/lock-screen');
+    ReactGA.event({
+      category: `Screen Change`,
+      action: `Set Screen to Locked`,
+    });
 
-		this.setState({ screen_locked: false });
-		localStorage.setItem('screen-locked', false);
-	};
+    document.getElementById('status-bar').blur();
+    setTimeout(() => {
+      this.setState({ screen_locked: true });
+    }, 100); // waiting for all windows to close (transition-duration)
+    localStorage.setItem('screen-locked', true);
+  };
 
-	changeBackgroundImage = (img_name) => {
-		this.setState({ bg_image_name: img_name });
-		localStorage.setItem('bg-image', img_name);
-	};
+  unLockScreen = () => {
+    ReactGA.pageview('/desktop');
 
-	shutDown = () => {
-		ReactGA.pageview('/switch-off');
-		ReactGA.event({
-			category: `Screen Change`,
-			action: `Switched off the Ubuntu`
-		});
+    window.removeEventListener('click', this.unLockScreen);
+    window.removeEventListener('keypress', this.unLockScreen);
 
-		document.getElementById('status-bar').blur();
-		this.setState({ shutDownScreen: true });
-		localStorage.setItem('shut-down', true);
-	};
+    this.setState({ screen_locked: false });
+    localStorage.setItem('screen-locked', false);
+  };
 
-	turnOn = () => {
-		ReactGA.pageview('/desktop');
+  changeBackgroundImage = (img_name) => {
+    this.setState({ bg_image_name: img_name });
+    localStorage.setItem('bg-image', img_name);
+  };
 
-		this.setState({ shutDownScreen: false, booting_screen: true });
-		this.setTimeOutBootScreen();
-		localStorage.setItem('shut-down', false);
-	};
+  shutDown = () => {
+    ReactGA.pageview('/switch-off');
+    ReactGA.event({
+      category: `Screen Change`,
+      action: `Switched off the Ubuntu`,
+    });
 
-	render() {
-		return (
-			<div className="w-screen h-screen overflow-hidden" id="monitor-screen">
-				<LockScreen
-					isLocked={this.state.screen_locked}
-					bgImgName={this.state.bg_image_name}
-					unLockScreen={this.unLockScreen}
-				/>
-				<BootingScreen
-					visible={this.state.booting_screen}
-					isShutDown={this.state.shutDownScreen}
-					turnOn={this.turnOn}
-				/>
-				<Navbar lockScreen={this.lockScreen} shutDown={this.shutDown} />
-				<Desktop bg_image_name={this.state.bg_image_name} changeBackgroundImage={this.changeBackgroundImage} />
-			</div>
-		);
-	}
+    document.getElementById('status-bar').blur();
+    this.setState({ shutDownScreen: true });
+    localStorage.setItem('shut-down', true);
+  };
+
+  turnOn = () => {
+    ReactGA.pageview('/desktop');
+
+    this.setState({ shutDownScreen: false, booting_screen: true });
+    this.setTimeOutBootScreen();
+    localStorage.setItem('shut-down', false);
+  };
+
+  render() {
+    return (
+      <div
+        className='w-screen h-screen overflow-hidden'
+        id='monitor-screen'
+        onKeyDown={this.loginWithoutAuth}
+        tabIndex='0'
+      >
+        <LockScreen
+          isLocked={this.state.screen_locked}
+          bgImgName={this.state.bg_image_name}
+          unLockScreen={this.unLockScreen}
+        />
+        <BootingScreen
+          visible={this.state.booting_screen}
+          isShutDown={this.state.shutDownScreen}
+          turnOn={this.turnOn}
+          remainingSeconds={this.state.remainingSeconds}
+          loginWithOAuth={this.loginWithAuth}
+        />
+        <Navbar lockScreen={this.lockScreen} shutDown={this.shutDown} />
+        <Desktop
+          bg_image_name={this.state.bg_image_name}
+          changeBackgroundImage={this.changeBackgroundImage}
+        />
+      </div>
+    );
+  }
 }
