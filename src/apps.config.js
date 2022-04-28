905b5e5a62fe6f6ac817b04c35b7f71c1e6e1d6e
何伟 <cnhwvista@gmail.com>
2022-04-28 22:06:52 +0800

    fix: update exam url

--- src/apps.config.js
+++ src/apps.config.js
@@ -205,7 +205,7 @@ const urls = {
         // eth.build url
         ethBuild: 'https://sandbox.eth.build/',
         // exam url
-        exam: 'http://lab.cyberemd.com/#/public/exam/list',
+        exam: 'http://lab.cyberemd.com/public/exam/list',
         // welight url
         welight: 'https://r8jmm3f9xe.feishu.cn/docs/doccnofLLkqPWZssHdo3u2wbD7c?from=from_copylink',
         // taishang-world-generator url
